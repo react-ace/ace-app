f3f6124228ebb63cb9bfb2e3876ce9ee555192b7
wlianfu <wlianfu@163.com>
2021-12-28 23:29:25 +0800

    customize theme

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 // @ts-ignore
 import { createRoot } from 'react-dom';
 
-import Home from './page/home/index';
+import AppRoot from '@subCon/app-root';
 
 const container = document.getElementById('root');
 
 const root = createRoot(container);
 
-root.render(<Home />);
-
+root.render(<AppRoot />);
